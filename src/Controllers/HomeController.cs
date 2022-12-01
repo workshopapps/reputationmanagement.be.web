16cbb0e5eacf672773768b94e09d6e528b8beba6
Best Ogbeide <ogbeidebest.ob@gmail.com>
2022-12-01 17:24:46 +0100

    Fully document the CRUD endpoints above with sample values and response codes and description

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -5,8 +5,10 @@ using Microsoft.AspNetCore.Mvc;
 using src.Entities;
 using src.Models;
 using src.Models.Dtos;
+using src.Models.ExampleModels;
 using src.Services;
 using Swashbuckle.AspNetCore.Annotations;
+using Swashbuckle.AspNetCore.Filters;
 using System.Security.Claims;
 
 namespace src.Controllers
@@ -78,8 +80,11 @@ namespace src.Controllers
         /// <param name="CreateReview"></param>
         /// <returns></returns>
         [SwaggerOperation(Summary = "Create a Review with this endpoint")]
-        [HttpPost("review")]
         [Authorize(Roles = "Customer", AuthenticationSchemes = "Bearer")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPost("review")]
         public ActionResult CreateReview([FromBody] ReviewForCreationDto reviewForCreationDto)
         {
             var review = _reviewRepo.CreateReviews(reviewForCreationDto);
@@ -93,9 +98,12 @@ namespace src.Controllers
         /// <returns>Review is successfully updated</returns>
 
         [SwaggerOperation(Summary = "Update a review by an User")]
-        [HttpPut]
         [Authorize(Roles = "Customer", AuthenticationSchemes = "Bearer")]
-        [Route("{reviewId}/edit")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPut("{reviewId}/edit")]
+        
         public ActionResult EditReview([FromBody] ReviewForUpdateDTO review)
         {
 
@@ -113,9 +121,11 @@ namespace src.Controllers
         /// <param name="reviewId"></param>
         /// <returns>Review is successfully deleted</returns>
         [SwaggerOperation(Summary = "delete a review by a user")]
-        [HttpDelete]
         [Authorize(Roles = "Customer", AuthenticationSchemes = "Bearer")]
-        [Route("{reviewId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpDelete("{reviewId}")]
         public ActionResult DeleteReview(Guid reviewId)
         {
 
@@ -135,8 +145,11 @@ namespace src.Controllers
         /// <param name="userId"></param>
         /// <returns>Reviews successfully deleted</returns>
         [SwaggerOperation(Summary = "delete multiple reviews by a User")]
-        [HttpDelete]
         [Authorize(Roles = "Customer", AuthenticationSchemes = "Bearer")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpDelete]
         [Route("{userId}")]
         public ActionResult DeleteReviews(Guid userId)
         {
---
+++ src/Models/ExampleModels/ReviewUpdateDetailsExample.cs
@@ -0,0 +1,36 @@
+﻿using src.Models.Dtos;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace src.Models.ExampleModels
+{
+    public class GoodReviewUpdateExampleDetailsForCustomer : IMultipleExamplesProvider<ReviewForUpdateDTO>
+    {
+        IEnumerable<SwaggerExample<ReviewForUpdateDTO>> IMultipleExamplesProvider<ReviewForUpdateDTO>.GetExamples()
+        {
+            yield return SwaggerExample.Create("Good input will return 200 + Review is successfully updated",
+
+                new ReviewForUpdateDTO()
+                {
+                   ReviewId = Guid.NewGuid(),
+                   ReviewString = "Bad product",
+                   Status =0,
+                   Priority =0
+                });
+        } 
+    }
+
+    public class BadReviewUpdateExampleDetailsForCustomer : IMultipleExamplesProvider<ReviewForUpdateDTO>
+    {
+        IEnumerable<SwaggerExample<ReviewForUpdateDTO>> IMultipleExamplesProvider<ReviewForUpdateDTO>.GetExamples()
+        {
+            yield return SwaggerExample.Create("Bad input that will return 400 BadRequest, \"The ReviewString field is required\"",
+               new ReviewForUpdateDTO()
+               {
+                   ReviewId = Guid.NewGuid(),
+                   ReviewString = "",
+                   Status = 0,
+                   Priority = 0
+               });
+        }
+    }
+}

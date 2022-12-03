241b8c487643d1afa317ba8aa2d9240c8475432f
Eseoghene Okoro <enerhen2013@gmail.com>
2022-12-03 09:28:47 +0100

    feat/BAC-71-Allow-user-claim-ticket

---
+++ src/Migrations/20221202084417_UpdateReview.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace src.Migrations
+{
+    public partial class UpdateReview : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "LawyerId",
+                table: "Reviews",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "ApplicationUser",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LawyerId",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "ApplicationUser");
+        }
+    }
+}

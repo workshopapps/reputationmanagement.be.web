aec88f76351e5d372c31047429856075c9cdf1ec
charliAlgO <charles.capitalgroup@gmail.com>
2022-12-03 15:41:48 -0800

    Documentation for the UpdateUser Endpoint

---
+++ src/Models/ExampleModels/UserUpdateDetailsExample.cs
@@ -0,0 +1,36 @@
+﻿using src.Models.Dtos;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace src.Models.ExampleModels
+{
+    public class GoodUserUpdateExampleDetailsForCustomer : IMultipleExamplesProvider<CustomerAccountForCreationDto>
+    {
+
+        public IEnumerable<SwaggerExample<CustomerAccountForCreationDto>> GetExamples()
+        {
+            yield return SwaggerExample.Create("Good input will return 200 + Review is successfully updated",
+
+                new CustomerAccountForCreationDto()
+                {
+                  Email = "usermail@gmail.com",
+                  BusinessEntityName = "userBuisinessName",
+                  Password = "UserPassword"
+                });
+        }
+    }
+
+    public class BadUserUpdateExampleDetailsForCustomer : IMultipleExamplesProvider<CustomerAccountForCreationDto>
+    {
+        public IEnumerable<SwaggerExample<CustomerAccountForCreationDto>> GetExamples()
+        {
+            yield return SwaggerExample.Create("Bad input will return 400 BadRequest, \"The field input is required\"",
+
+                new CustomerAccountForCreationDto()
+                {
+                    Email = "usermail@gmail.com",
+                    BusinessEntityName = "userBuisinessName",
+                    Password = "UserPassword"
+                });
+        }
+    }
+}

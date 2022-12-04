c76db962ea494ec01cd0583c5294f18fe319a4c3
Joseph Ibochi <josephibochi@gmail.com>
2022-12-04 10:55:52 +0100

    fix(BE-86) Endpoint to store responses from the form on the career page

---
+++ src/Helpers/EnumString.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace src.Helpers
+{
+    public class EnumString
+    {
+        public static string GetEnumDescription(Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] attributes = fi.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+
+            if (attributes != null && attributes.Any())
+            {
+                return attributes.First().Description;
+            }
+            return value.ToString();
+        }
+    }
+}

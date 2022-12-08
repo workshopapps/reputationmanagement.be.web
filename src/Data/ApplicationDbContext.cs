7f093873a1d7489e15801b1090da39cae4edd59e
David Okeke <davidokeke.c@gmail.com>
2022-12-08 13:12:55 +0100

    comment out seed data for live server

--- src/Data/ApplicationDbContext.cs
+++ src/Data/ApplicationDbContext.cs
@@ -8,23 +8,23 @@ namespace src.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options) { }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-                var imagePathsInImageUploadFolder = Directory.GetFiles(Path.Combine(Environment.CurrentDirectory, "Uploads", "BlogImageUpload"));
-                foreach (var imagePath in imagePathsInImageUploadFolder)
-                {
-                    modelBuilder.Entity<BlogEntry>().HasData(
-                            new BlogEntry
-                            {
-                                PathToImage = imagePath,
-                                Title = "Sample Title",
-                                Description = "A black/white painting of a monkey",
-                                CreationDate = DateTime.Now,
-                                Url = "https://notion.com"
-                            });
-                }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //        var imagePathsInImageUploadFolder = Directory.GetFiles(Path.Combine(Environment.CurrentDirectory, "Uploads", "BlogImageUpload"));
+        //        foreach (var imagePath in imagePathsInImageUploadFolder)
+        //        {
+        //            modelBuilder.Entity<BlogEntry>().HasData(
+        //                    new BlogEntry
+        //                    {
+        //                        PathToImage = imagePath,
+        //                        Title = "Sample Title",
+        //                        Description = "A black/white painting of a monkey",
+        //                        CreationDate = DateTime.Now,
+        //                        Url = "https://notion.com"
+        //                    });
+        //        }
             
-        }
+        //}
 
         public DbSet<Review> Reviews { get; set; }
         public DbSet<UserComplains> UserComplaint { get; set; }

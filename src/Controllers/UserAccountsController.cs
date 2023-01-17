ea8d197110131fdf652ce316d6f8584ce311b1ed
David Okeke <davidokeke.c@gmail.com>
2023-01-17 10:06:49 +0100

    Fix issue with google signin

--- src/Controllers/UserAccountsController.cs
+++ src/Controllers/UserAccountsController.cs
@@ -257,7 +257,7 @@ namespace src.Controllers
             if (payload == null)
                 return BadRequest("Invalid External Authentication.");
 
-            var info = new UserLoginInfo(externalAuth.Provider, payload.Subject, externalAuth.BusinessName);
+            var info = new UserLoginInfo(externalAuth.Provider, payload.Subject, externalAuth.Provider);
 
             var user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
             if (user == null)
